18ceae1784dec1550be3f8d6f41cd9d46e4ceb87
abida <jeanarnolde1995@gmail.com>
2020-09-26 02:14:17 +0100

    init project

---
+++ schoolManagementV1/src/main/java/com/sm/schoolManagement/SchoolManagementV1Application.java
@@ -0,0 +1,13 @@
+package com.sm.schoolManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementV1Application.class, args);
+	}
+
+}
